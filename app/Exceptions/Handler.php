81d93e8a3e17be7d49a7a27fc13c27da79e75b0d
dev-kimreo0301 <wkim0301@cocen.com>
2021-04-30 09:39:46 +0900

    fix: handler 초기 상태로 원복

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,16 +2,9 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Auth\Access\AuthorizationException;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Spatie\Permission\Exceptions\UnauthorizedException;
-use Laravel\Passport\Exceptions\OAuthServerException;
 use Throwable;
 
-/**
- * Class Handler.
- */
 class Handler extends ExceptionHandler
 {
     /**
@@ -20,7 +13,7 @@ class Handler extends ExceptionHandler
      * @var array
      */
     protected $dontReport = [
-        GeneralException::class,
+        //
     ];
 
     /**
@@ -35,58 +28,14 @@ class Handler extends ExceptionHandler
     ];
 
     /**
-     * Report or log an exception.
+     * Register the exception handling callbacks for the application.
      *
-     * @param  \Throwable  $exception
      * @return void
-     *
-     * @throws \Throwable
      */
-    public function report(Throwable $exception)
+    public function register()
     {
-        parent::report($exception);
-    }
-
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Throwable  $exception
-     * @return \Symfony\Component\HttpFoundation\Response
-     *
-     * @throws \Throwable
-     */
-    public function render($request, Throwable $exception)
-    {
-        if ($exception instanceof UnauthorizedException) {
-            return redirect()
-                ->route(homeRoute())
-                ->withFlashDanger(__('You do not have access to do that.'));
-        }
-
-        if ($exception instanceof OAuthServerException) {
-//            echo '<xmp>';
-//            print_r($exception);
-//            echo '</xmp>';
-//            return response()->json([
-//                'error' => $exception->getErrorType(),
-//                'message' => $exception->getMessage(),
-//                'hint' => $exception->getHint(),
-//            ], $exception->getHttpStatusCode());
-        }
-
-        if ($exception instanceof AuthorizationException) {
-            return redirect()
-                ->back()
-                ->withFlashDanger($exception->getMessage() ?? __('You do not have access to do that.'));
-        }
-
-        if ($exception instanceof ModelNotFoundException) {
-            return redirect()
-                ->route(homeRoute())
-                ->withFlashDanger(__('The requested resource was not found.'));
-        }
-
-        return parent::render($request, $exception);
+        $this->reportable(function (Throwable $e) {
+            //
+        });
     }
 }
