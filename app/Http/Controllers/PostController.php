b8ec7f426bffd66a1fb0ced4fa8bec206d74b3ec
dev-kimreo0301 <wkim0301@cocen.com>
2021-05-06 10:38:45 +0900

    refac: PostController 에 funcGetInfo 함수 제거

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -706,78 +706,7 @@ class PostController extends Controller
     }
 
 
-    /**
-     * @param $postId
-     * @param $boardId
-     * @return mixed
-     */
-    public function funcGetInfo($postId, $boardId = 0)
-    {
-        try {
-
-            // 게시판 번호가 없을 경우
-            if (!$boardId) {
-                $boardId = $this->post->select('board_id')->where('id', $postId)->first()['board_id'];
-            }
-
-            // 게시판 정보
-            $boardCollect = $this->boardService->getInfo($boardId);
-            $boardInfo = $boardCollect->toArray();
-
-            // 데이터 cache
-            $tags = separateTag('board.' . $boardId . '.post.' . $postId);
-            $data = Cache::tags($tags)->remember('info', config('cache.custom.expire.common'), function () use ($postId, $boardId, $boardInfo) {
-                $select = ['posts.id', 'title', 'board_id', 'content', 'hidden', 'posts.etc', 'posts.user_id', 'posts.created_at', 'posts.updated_at'];
-
-                // 섬네일 지원 게시판일 경우
-                if ($boardInfo['options']['thumbnail']) {
-                    $select[] = 'af.url AS thumbnail';
-                    $select[] = 'af.id AS thumbNo';
-                }
-
-                // 게시글 답변 지원 게시판 일 경우
-                if ($boardInfo['options']['boardReply']) {
-                    $select[] = 'comment';
-                }
-
-                $post = $this->post->select($select)->where(['posts.id' => $postId, 'board_id' => $boardId]);
-
-                // 섬네일 사용
-                if ($boardInfo['options']['thumbnail']) {
-                    $post = $post->leftjoin('attach_files AS af', function ($join) {
-                        $join
-                            ->on('posts.id', '=', 'af.type_id')
-                            ->where('type', $this->attachType)
-                            ->whereJsonContains('af.etc', ['type' => 'thumbnail']);
-                    });
-                }
-
-                $post = $post->first();
-
-                $post->thumbnail = [
-                    'id' => $post->thumbNo,
-                    'url' => $post->thumbnail
-                ];
-                unset($post->thumbNo);
-
-                // 기타정보 가공
-                if (isset($post->etc['status'])) {
-                    $post->status = __('common.post.etc.status.' . $post->etc['status']);
-                }
-
-                // 게시글 추가 정보 (회원)
-                $post->userName = $post->user->toArray()['name'];
-                unset($post->user);
-
-                return $post;
-            });
-
-            return $data;
-        } catch (\Throwable $e) {
-            return response()->json(getResponseError($e->getMessage()), $e->getCode());
-        }
-    }
-
+    
     public function test(Request $request)
     {
 
