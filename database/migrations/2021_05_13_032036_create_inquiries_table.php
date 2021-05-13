3ab589cb1bbcbc9f1f9c500b360b03bc8b3137a0
dev-kimreo0301 <wkim0301@cocen.com>
2021-05-13 12:27:16 +0900

    feat: inquiries, inquiry_answers 테이블 migration 작성

---
+++ database/migrations/2021_05_13_032036_create_inquiries_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInquiriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inquiries', function (Blueprint $table) {
+            $table->collation = 'utf8mb4_general_ci';
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('title', 100);
+            $table->mediumText('question');
+            $table->string('status', 16)->default('wating');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inquiries');
+    }
+}
