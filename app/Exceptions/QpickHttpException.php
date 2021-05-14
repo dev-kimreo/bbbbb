6524f07aadce505088a379c0aadf7f2900a25d0c
Jaewon Kim <epiloum@cocen.com>
2021-05-14 12:05:46 +0900

    feat: remove logic for integer error code

--- app/Exceptions/QpickHttpException.php
+++ app/Exceptions/QpickHttpException.php
@@ -26,43 +26,27 @@ class QpickHttpException extends Exception
         return $this->errors;
     }
 
-    protected function getErrorInfo($code, ...$key) {
+    protected function getErrorInfo($code, $key) {
         // Getting Translated Message
-        $msg = __(is_integer($code)? config('subError.' . $code): 'messages.' . $code);
+        $msg = __('messages.' . $code);
 
         // Invalid message
         if(!$msg || $msg == $code) {
-            return [
-                'code' => 'unknown',
-                'message' => 'There is no matched message.'
-            ];
-        }
-
-        // Replacing
-        preg_match_all("/:{1}[^\s]+[a-z]+/", $msg, $matchArrs);
-
-        foreach ($matchArrs[0] as $k => $attr) {
-            if ( is_array($key) && count($key) ) {
-                if ( isset($key[$k]) && $key[$k] ) {
-                    if (preg_match("/^\{{1}[^\{\}]+\}{1}$/", $key[$k])) {
-                        $key[$k] = preg_replace('/\{|\}/', '', $key[$k]);
-                        $msg = str_replace($attr, $key[$k], $msg);
-                    } else {
-                        $msg = str_replace($attr, __('words.' . $key[$k]), $msg);
-                    }
-                }
-            }
+            $msg = 'No corresponding message for the given code.';
         }
 
         // Generating Response Data
-        $res = [
-            'code' => $code,
-            'target' => $key[0]?? false,
-            'message' => $msg
-        ];
-
-        if($res['target'] === false) {
-            unset($res['target']);
+        if($key) {
+            $res = [
+                'code' => $code,
+                'target' => $key ?? false,
+                'message' => $msg
+            ];
+        } else {
+            $res = [
+                'code' => $code,
+                'message' => $msg
+            ];
         }
 
         // Return
