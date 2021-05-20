36cefce24b168e5a02199aa4eec4092ab6e28bbb
dev-kimreo0301 <wkim0301@cocen.com>
2021-05-20 13:49:18 +0900

    refac: InquiryContoller Swagger 정리 및 Exceptions 통일

--- app/Http/Controllers/InquiryController.php
+++ app/Http/Controllers/InquiryController.php
@@ -39,6 +39,51 @@ class InquiryController extends Controller
     }
 
 
+
+    /**
+     * @OA\Schema (
+     *      schema="inquiryCreate",
+     *      required={"title", "question"},
+     *      @OA\Property(property="title", type="string", example="1:1 문의 제목입니다.", description="1:1 문의 제목" ),
+     *      @OA\Property(property="question", type="string", example="1:1 문의 내용입니다.", description="1:! 문의 내용" )
+     *  )
+     */
+
+
+    /**
+     * @OA\Post(
+     *      path="/v1/inquiry",
+     *      summary="1:1문의 작성",
+     *      description="1:1문의 작성",
+     *      operationId="inquiryCreate",
+     *      tags={"1:1문의"},
+     *      @OA\RequestBody(
+     *          required=true,
+     *          description="",
+     *          @OA\JsonContent(
+     *              ref="#/components/schemas/inquiryCreate"
+     *          ),
+     *      ),
+     *      @OA\Response(
+     *          response=201,
+     *          description="created",
+     *          @OA\JsonContent(
+     *              ref="#/components/schemas/Inquiry"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="forbidden"
+     *      ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="failed"
+     *      ),
+     *      security={{
+     *          "davinci_auth":{}
+     *      }}
+     *  )
+     */
     public function store(CreateRequest $request)
     {
         // Bacckoffice login
@@ -57,10 +102,55 @@ class InquiryController extends Controller
             $this->inquiry->created_at = Carbon::now();
             $this->inquiry->save();
 
-            return response()->json($this->inquiry, 201);
+            $this->inquiry->refresh();
+
+            return response()->json(CollectionLibrary::toCamelCase(collect($this->inquiry)), 201);
         }
     }
 
+
+    /**
+     * @OA\Schema (
+     *      schema="inquiryList",
+     *      @OA\Property(property="page", type="integer", example=1, default=1, description="페이지" ),
+     *      @OA\Property(property="perPage", type="integer", example=15, description="한 페이지당 보여질 갯 수" )
+     * )
+     *
+     * @OA\Get(
+     *      path="/v1/inquiry",
+     *      summary="1:1문의 목록",
+     *      description="1:1문의 목록",
+     *      operationId="inquiryGetList",
+     *      tags={"1:1문의"},
+     *      @OA\RequestBody(
+     *          required=true,
+     *          description="",
+     *          @OA\JsonContent(
+     *              ref="#/components/schemas/inquiryList"
+     *          ),
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="successfully",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="header", type="object", ref="#/components/schemas/Pagination" ),
+     *              @OA\Property(property="list", type="array",
+     *                  @OA\Items(type="object",
+     *                      ref="#/components/schemas/Inquiry"
+     *                  )
+     *              ),
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="failed"
+     *      ),
+     *      security={{
+     *          "davinci_auth":{},
+     *          "admin_auth":{}
+     *      }}
+     *  )
+     */
     public function index(IndexRequest $request)
     {
         //  목록
@@ -73,6 +163,12 @@ class InquiryController extends Controller
         if (auth()->user()->isLoginToManagerService()) {
 
         } else {
+
+            // check viewAny Policy
+            if (!auth()->user()->can('viewAny', [$this->inquiry])) {
+                throw new QpickHttpException(403, 'common.unauthorized');
+            }
+
             // where 절
             $whereModel = $this->inquiry->where(['user_id' => auth()->user()->id]);
 
@@ -82,11 +178,22 @@ class InquiryController extends Controller
             // 문의 정보
             $inquiry =
                 $whereModel
+                    ->with('user')
                     ->skip($pagination['skip'])
                     ->take($pagination['perPage'])
                     ->orderBy('id', 'desc');
 
             $data = $inquiry->get();
+
+
+            // 데이터 가공
+            $data->each(function (&$v) use ($set) {
+                // 유저 이름
+                if ($v->user) {
+                    $v->userName = $v->user->toArray()['name'];
+                    unset($v->user);
+                }
+            });
         }
 
         $result = [
@@ -97,6 +204,38 @@ class InquiryController extends Controller
         return response()->json(CollectionLibrary::toCamelCase(collect($result)), 200);
     }
 
+
+    /**
+     * @OA\Get(
+     *      path="/v1/inquiry/{id}",
+     *      summary="1:1문의 상세",
+     *      description="1:1문의 상세",
+     *      operationId="inquiryGetInfo",
+     *      tags={"1:1문의"},
+     *      @OA\Response(
+     *          response=200,
+     *          description="successfully",
+     *          @OA\JsonContent(
+     *              allOf={
+     *                   @OA\Schema(ref="#/components/schemas/Inquiry"),
+     *                   @OA\Schema(
+     *                      @OA\Property(property="attachFiles", type="array",
+     *                          @OA\Items(ref="#/components/schemas/AttachFile")
+     *                      )
+     *                   )
+     *              }
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="failed"
+     *      ),
+     *      security={{
+     *          "davinci_auth":{},
+     *          "admin_auth":{}
+     *      }}
+     *  )
+     */
     public function show($id, ShowRequest $request)
     {
         // Bacckoffice login
@@ -115,13 +254,53 @@ class InquiryController extends Controller
                 ])
                 ->first();
             if (!$collect) {
-                throw new QpickHttpException(422, 'common.not_found');
+                throw new QpickHttpException(404, 'common.not_found');
             }
-        }
 
-        return response()->json(CollectionLibrary::toCamelCase(collect($collect)), 200);
+            // check view Policy
+            if (!auth()->user()->can('view', [$collect])) {
+                throw new QpickHttpException(403, 'common.unauthorized');
+            }
+
+            return response()->json(CollectionLibrary::toCamelCase(collect($collect)), 200);
+        }
     }
 
+
+    /**
+     * @OA\Patch(
+     *      path="/v1/inquiry/{id}",
+     *      summary="1:1문의 수정",
+     *      description="1:1문의 수정",
+     *      operationId="inquiryModify",
+     *      tags={"1:1문의"},
+     *      @OA\RequestBody(
+     *          required=true,
+     *          description="",
+     *          @OA\JsonContent(
+     *              ref="#/components/schemas/inquiryCreate"
+     *          ),
+     *      ),
+     *      @OA\Response(
+     *          response=201,
+     *          description="modified",
+     *          @OA\JsonContent(
+     *              ref="#/components/schemas/Inquiry"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="forbidden"
+     *      ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="failed"
+     *      ),
+     *      security={{
+     *          "davinci_auth":{}
+     *      }}
+     *  )
+     */
     public function update($id, UpdateRequest $request)
     {
         // Bacckoffice login
@@ -133,7 +312,11 @@ class InquiryController extends Controller
                 ->where(['id' => $id, 'user_id' => auth()->user()->id])
                 ->first();
             if (!$collect) {
-                throw new QpickHttpException(422, 'common.not_found');
+                throw new QpickHttpException(404, 'common.not_found');
+            }
+
+            if (!auth()->user()->can('update', [$collect])) {
+                throw new QpickHttpException(403, 'common.unauthorized');
             }
 
             $collect->title = $request->title ?? $collect->title;
@@ -149,6 +332,35 @@ class InquiryController extends Controller
         }
     }
 
+
+    /**
+     * @OA\delete(
+     *      path="/v1/inquiry/{id}",
+     *      summary="1:1문의 삭제",
+     *      description="1:1문의 삭제",
+     *      operationId="inquiryDelete",
+     *      tags={"1:1문의"},
+     *      @OA\Response(
+     *          response=204,
+     *          description="deleted"
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="forbidden"
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="not found"
+     *      ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="failed"
+     *      ),
+     *      security={{
+     *          "davinci_auth":{}
+     *      }}
+     *  )
+     */
     public function destroy($id, DestroyRequest $request)
     {
         // Bacckoffice login
@@ -163,7 +375,11 @@ class InquiryController extends Controller
                 ])
                 ->first();
             if (!$collect) {
-                throw new QpickHttpException(422, 'common.not_found');
+                throw new QpickHttpException(404, 'common.not_found');
+            }
+
+            if (!auth()->user()->can('delete', [$collect])) {
+                throw new QpickHttpException(403, 'common.unauthorized');
             }
 
             // 첨부파일 삭제
