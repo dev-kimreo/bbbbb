757ffde4ebc77e4409a8bf1a1aa30227683d471a
dev-kimreo0301 <wkim0301@cocen.com>
2021-05-20 13:40:28 +0900

    refac: User Model swagger 작성, remember_token 필드 hidden 추가

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,16 +14,16 @@ use Laravel\Passport\HasApiTokens;
  *
  * @OA\Schema(
  * required={"password"},
- * @OA\Xml(name="User1"),
  * @OA\Property(property="id", type="integer", readOnly="true", example="1"),
- * @OA\Property(property="role", type="string", readOnly="true", description="User role"),
- * @OA\Property(property="email", type="string", readOnly="true", format="email", description="User unique email address", example="user@gmail.com"),
- * @OA\Property(property="email_verified_at", type="string", readOnly="true", format="date-time", description="Datetime marker of verification status", example="2019-02-25 12:59:20"),
- * @OA\Property(property="first_name", type="string", maxLength=32, example="John"),
- * @OA\Property(property="last_name", type="string", maxLength=32, example="Doe"),
- * @OA\Property(property="created_at", ref="#/components/schemas/Base/properties/created_at"),
- * @OA\Property(property="updated_at", ref="#/components/schemas/Base/properties/updated_at"),
- * @OA\Property(property="deleted_at", ref="#/components/schemas/Base/properties/deleted_at")
+ * @OA\Property(property="email", type="string", readOnly="true", format="email", description="회원 email", example="user@qpicki.com"),
+ * @OA\Property(
+ *     property="emailVerifiedAt", type="string", readOnly="true", format="date-time",
+ *     description="회원 이메일 인증 일자", default=null, example="2019-02-25 12:59:20"
+ * ),
+ * @OA\Property(property="name", type="string", maxLength=255, example="홍길동"),
+ * @OA\Property(property="grade", type="integer", default=1, description="0:준회원, 1:정회원", example=1),
+ * @OA\Property(property="createdAt", ref="#/components/schemas/Base/properties/created_at"),
+ * @OA\Property(property="updatedAt", ref="#/components/schemas/Base/properties/updated_at"),
  * )
  *
  * Class User
@@ -56,7 +56,8 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var array
      */
     protected $hidden = [
-        'password'
+        'password',
+        'remember_token'
     ];
 
     public function checkAdmin()
@@ -76,9 +77,7 @@ class User extends Authenticatable implements MustVerifyEmail
 
     public function getEmailVerifiedAtAttribute($value)
     {
-        if (isset($value)) {
-            return Carbon::parse($value)->format('c');
-        }
+        return $value ? Carbon::parse($value)->format('c') : $value;
     }
 
     public function getCreatedAtAttribute($value)
