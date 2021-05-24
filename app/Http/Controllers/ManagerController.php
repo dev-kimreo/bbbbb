d4aec3fc2abbef0aada4571573fbe628cc7399c3
Jaewon Kim <epiloum@cocen.com>
2021-05-24 11:12:58 +0900

    refac: add a swagger document about manager

--- app/Http/Controllers/ManagerController.php
+++ app/Http/Controllers/ManagerController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Libraries\CollectionLibrary;
+use App\Libraries\PaginationLibrary;
+use Illuminate\Http\Request;
 use App\Models\{Manager, User, Authority};
 use App\Http\Requests\Members\Managers\StoreManagerRequest;
 use Illuminate\Support\Collection;
@@ -18,15 +20,77 @@ class ManagerController extends Controller
     }
 
     /**
+     * @OA\Get(
+     *      path="/v1/manager",
+     *      summary="관리자 목록",
+     *      description="관리자의 전체목록을 표시합니다",
+     *      operationId="managerGetList",
+     *      tags={"관리자"},
+     *      @OA\RequestBody(
+     *          required=true,
+     *          description="",
+     *          @OA\JsonContent(
+     *              required={}
+     *          ),
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="successfully",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="header", type="object", ref="#/components/schemas/Pagination" ),
+     *              @OA\Property(property="list", type="array",
+     *                  @OA\Items(ref="#/components/schemas/Manager")
+     *              )
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="failed"
+     *      )
+     *  )
+     */
+    /**
      * Display a listing of the resource.
      *
-     * @return Collection
+     * @return array
      */
-    public function index(): Collection
+    public function index(Request $request): array
     {
-        return CollectionLibrary::toCamelCase($this->manager::all());
+        $data = $this->manager::all();
+
+        return [
+            'header' => PaginationLibrary::set($request->page, $data->count(), $request->perPage),
+            'list' => CollectionLibrary::toCamelCase($data)
+        ];
     }
 
+    /**
+     * @OA\Post (
+     *      path="/v1/manager",
+     *      summary="관리자 등록",
+     *      description="기존 사용자를 새로운 관리자로 지정합니다",
+     *      operationId="managerCreate",
+     *      tags={"관리자"},
+     *      @OA\RequestBody(
+     *          required=true,
+     *          description="",
+     *          @OA\JsonContent(
+     *              required={"user_id", "authority_id"},
+     *              @OA\Property(property="user_id", type="integer", example="2", description="사용자의 고유번호(PK)"),
+     *              @OA\Property(property="authority_id", type="integer", example="5", description="관리자그룹의 고유번호(PK)")
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="successfully",
+     *          @OA\JsonContent(ref="#/components/schemas/Manager")
+     *      ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="failed"
+     *      )
+     *  )
+     */
     /**
      * Store a newly created resource in storage.
      *
@@ -50,6 +114,35 @@ class ManagerController extends Controller
     }
 
     /**
+     * @OA\Get(
+     *      path="/v1/manager/{id}",
+     *      summary="관리자 상세",
+     *      description="1명의 관리자에 대한 상세정보를 표시합니다",
+     *      operationId="managerGetInfo",
+     *      tags={"관리자"},
+     *      @OA\RequestBody(
+     *          required=true,
+     *          description="",
+     *          @OA\JsonContent(
+     *              required={}
+     *          ),
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="successfully",
+     *          @OA\JsonContent(ref="#/components/schemas/Manager")
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="unauthenticated"
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="not found"
+     *      )
+     *  )
+     */
+    /**
      * Display the specified resource.
      *
      * @param  int  $id
@@ -60,6 +153,35 @@ class ManagerController extends Controller
         return CollectionLibrary::toCamelCase(collect($this->manager::findOrFail($id)));
     }
 
+    /**
+     * @OA\Delete(
+     *      path="/v1/manager/{id}",
+     *      summary="관리자 삭제",
+     *      description="기존에 등록된 관리자를 삭제합니다",
+     *      operationId="managerDelete",
+     *      tags={"관리자"},
+     *      @OA\RequestBody(
+     *          required=true,
+     *          description=""
+     *      ),
+     *      @OA\Response(
+     *          response=204,
+     *          description="successfully"
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="unauthenticated"
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="not found"
+     *      ),
+     *      security={{
+     *          "davinci_auth":{}
+     *      }}
+     *  )
+     */
+    
     /**
      * Remove the specified resource from storage.
      *
--- app/Models/Manager.php
+++ app/Models/Manager.php
@@ -7,7 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Carbon\Carbon;
 
-
+/**
+ *
+ * @OA\Schema(
+ * @OA\Property(property="id", type="integer", readOnly="true", example="1"),
+ * @OA\Property(property="user_id", type="integer", readOnly="true", description="사용자의 고유번호(PK)", example="1"),
+ * @OA\Property(property="authority_id", type="integer", readOnly="true", description="관리자그룹의 고유번호(PK)", example="1"),
+ * @OA\Property(
+ *     property="createdAt", type="string", readOnly="true", format="date-time",
+ *     ref="#/components/schemas/Base/properties/created_at", example="2021-02-25 12:59:20"
+ * ),
+ * @OA\Property(property="user", type="object", readOnly="true", ref="#/components/schemas/User"),
+ * @OA\Property(property="authority", type="object", readOnly="true", ref="#/components/schemas/Authority")
+ * )
+ *
+ */
 class Manager extends Model
 {
     use HasFactory, SoftDeletes;
