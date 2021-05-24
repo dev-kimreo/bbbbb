ea7ab1c99d22175f1d10663f3b8f22b4044a4159
dev-kimreo0301 <wkim0301@cocen.com>
2021-05-24 10:55:42 +0900

    refac: AttachController C 메소드 store 로 변경

--- app/Http/Requests/Attaches/StoreRequest.php
+++ app/Http/Requests/Attaches/StoreRequest.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use App;
 
-class CreateRequest extends FormRequest
+class StoreRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
