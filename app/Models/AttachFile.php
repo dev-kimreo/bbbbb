ee11094719b9e143a7fd7817b3524e9250ef27a1
dev-kimreo0301 <wkim0301@cocen.com>
2021-05-24 10:55:04 +0900

    refac: attachFiles Model Soft delete 추가

--- app/Models/AttachFile.php
+++ app/Models/AttachFile.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Carbon\Carbon;
 
 
@@ -29,7 +30,7 @@ use Carbon\Carbon;
  */
 class AttachFile extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
