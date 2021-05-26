eecc259261d2115e682e6e5502b42d02a9f62071
dev-kimreo0301 <wkim0301@cocen.com>
2021-05-26 10:32:14 +0900

    feat: getBySort method 추가 (sortBy 파라미터를 통해 특정 필드를 +(ASC), -(DESC) 할 수 있도록 만듬)

--- app/Libraries/CollectionLibrary.php
+++ app/Libraries/CollectionLibrary.php
@@ -2,6 +2,7 @@
 
 namespace App\Libraries;
 
+use App\Exceptions\QpickHttpException;
 use Str;
 use Illuminate\Support\Collection;
 
@@ -19,7 +20,7 @@ class CollectionLibrary
 
         if (is_array($value) || is_object($value)) {
             foreach ($value as $key => $val) {
-                if ( is_array($val) || is_object($val) ) {
+                if (is_array($val) || is_object($val)) {
                     $res[Str::camel($key)] = self::toCamelCase(collect($val));
                 } else {
                     $res[Str::camel($key)] = $val;
@@ -28,6 +29,31 @@ class CollectionLibrary
         }
 
         return collect($res);
+    }
+
+    public static function getBySort(string $sort, array $possibleKey = []): Collection
+    {
+        $res = collect();
+        $sortExp = explode(',', $sort);
+        foreach ($sortExp as $v) {
+            if ( !isset($v) || !$v ) {
+                continue;
+            }
+
+            $key = preg_replace("/\-|\+|\s/", '', $v);
+
+            if (!in_array($key, $possibleKey)) {
+                throw new QpickHttpException(422, 'common.bad_request', 'sortBy.' . $key);
+            }
+
+            if(Str::contains($v, '-')) {
+                $res->push(['key' => $key, 'value' => 'DESC']);
+            } else{
+                $res->push(['key' => $key, 'value' => 'ASC']);
+            }
+        }
+
+        return $res;
     }
 
 }
