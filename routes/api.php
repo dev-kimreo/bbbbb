c4d303b20115db207d1c9dbd1add902a44593cd5
Jaewon Kim <epiloum@cocen.com>
2021-05-31 17:43:56 +0900

    refac: clean up routers according to CRUD

--- routes/api.php
+++ routes/api.php
@@ -37,164 +37,85 @@ Route::group([
     'prefix' => 'v1',
     'middleware' => 'language'
 ], function () {
-
     /**
      * 회원 관련
      */
-    Route::group([
-        'prefix' => 'user'
-    ], function () {
-
+    Route::group(['prefix' => 'user', 'middleware' => 'auth:api'], function () {
         // 회원 관련 CRUD
-        Route::post('', [MemberController::class, 'store']);
+        Route::post('', [MemberController::class, 'store'])->withoutmiddleware('auth:api');
+        Route::get('', [MemberController::class, 'index'])->middleware('admin');
+        Route::get('/{id}', [MemberController::class, 'show'])->middleware('admin')->where(['id' => '[0-9]+']);
+        Route::patch('/{id}', [MemberController::class, 'update'])->where(['id' => '[0-9]+']);
 
         // 회원 세션 CRUD
-        Route::post('/auth', [AccessTokenController::class, 'store']);
-
-
-        // 인증 필요
-        Route::group([
-            'middleware' => ['auth:api'],
-        ], function () {
-            // 회원 관련 CRUD
-            Route::patch('/{id}', [MemberController::class, 'update']);
-
-            // 회원 세션 CRUD
-            Route::get('/auth', [AccessTokenController::class, 'show']);
-            Route::delete('/auth', [AccessTokenController::class, 'destroy']);
-
-
-            Route::group([
-                'middleware' => ['admin'],
-            ], function(){
-                // 회원 관련 CRUD
-                Route::get('', [MemberController::class, 'index']);
-                Route::get('/{id}', [MemberController::class, 'show'])->where(['id' => '[0-9]+']);
-            });
-
-
-
-        });
-
-
-        // 이메일 재 인증 관련
-        Route::group([
-            'prefix' => 'email-verification'
-        ], function () {
-            // 이메일 인증 route
-            Route::get('/{verifyKey}/{id}', [MemberController::class, 'verification'])->name('verification.verify');
-
-            // 인증 필요
-            Route::group([
-                'middleware' => 'auth:api',
-            ], function () {
-                // 이메일 인증 재발송
-                Route::post('', [MemberController::class, 'resendVerificationEmail']);
-            });
-        });
-
-
-        /**
-         * 비밀번호 관련
-         */
-        Route::group([
-            'prefix' => 'password',
-        ], function () {
-
-            /**
-             * 비밀번호 찾기 프로세스
-             */
-            Route::group([
-                'prefix' => 'reset-mail'
-            ], function () {
-                // 비밀번호 변경 링크 유효성 체크
-                Route::get('', [MemberController::class, 'changePwdVerification']);
-
-                // 비밀번호 변경 링크 발송
-                Route::post('', [MemberController::class, 'passwordResetSendLink']);
-
-                // 비밀번호 변경 링크 발송 후 변경
-                Route::patch('', [MemberController::class, 'passwordReset']);
-            });
-
-            // 인증 필요
-            Route::group([
-                'middleware' => 'auth:api',
-            ], function () {
-                // 비밀번호 검증
-                Route::post('', [MemberController::class, 'checkPassword']);
-
-                // 비밀번호 변경
-                Route::patch('', [MemberController::class, 'modifyPassword']);
-            });
-
-        });
-
-
+        Route::post('/auth', [AccessTokenController::class, 'store'])->withoutmiddleware('auth:api');
+        Route::get('/auth', [AccessTokenController::class, 'show']);
+        Route::delete('/auth', [AccessTokenController::class, 'destroy']);
     });
 
-
-    // 메일
-    Route::group([
-        'prefix' => 'email'
-    ], function ($router) {
+    /**
+     * 회원 / 이메일 인증
+     */
+    Route::group(['prefix' => 'user/email-verification'], function () {
+        // 이메일 인증링크 재발송 & 인증링크 클릭시 랜딩 페이지
+        Route::post('', [MemberController::class, 'resendVerificationEmail'])->middleware('auth:api');
+        Route::get('/{verifyKey}/{id}', [MemberController::class, 'verification'])->name('verification.verify');
     });
 
+    /**
+     * 회원 / 비밀번호 관련
+     */
+    Route::group(['prefix' => 'user/password'], function () {
+        // 비밀번호 변경 링크 발송 & 유효성 체크 & 발송 후 변경
+        Route::post('/reset-mail', [MemberController::class, 'passwordResetSendLink']);
+        Route::get('/reset-mail', [MemberController::class, 'changePwdVerification']);
+        Route::patch('/reset-mail', [MemberController::class, 'passwordReset']);
 
-    // 관리자 및 권한 관련
-    Route::group([
-            'middleware' => 'auth:api',
-    ], function () {
+        // 비밀번호 검증 & 비밀번호 변경
+        Route::post('', [MemberController::class, 'checkPassword'])->middleware('auth:api');
+        Route::patch('', [MemberController::class, 'modifyPassword'])->middleware('auth:api');
+    });
+
+    /**
+     * 관리자 및 권한 관련
+     */
+    Route::group(['middleware' => 'auth:api'], function () {
         Route::resource('authority', AuthorityController::class);
         Route::resource('manager', ManagerController::class,[
             'only' => ['index', 'show', 'store', 'destroy']
         ]);
     });
 
-    // 게시판 관련
-    Route::group([
-        'prefix' => 'board'
-    ], function () {
+    /**
+     * 게시판 관련
+     */
+    Route::group(['prefix' => 'board', 'middleware' => 'auth:api'], function () {
         // 게시판 CRUD
-        Route::get('', [BoardController::class, 'index']);
-        Route::get('/{id}', [BoardController::class, 'show'])->where(['id' => '[0-9]+']);
+        Route::post('', [BoardController::class, 'store']);
+        Route::get('', [BoardController::class, 'index'])->withoutmiddleware('auth:api');
+        Route::get('/{id}', [BoardController::class, 'show'])->withoutmiddleware('auth:api')->where(['id' => '[0-9]+']);
+        Route::patch('/{id}', [BoardController::class, 'update']);
+        Route::delete('/{id}', [BoardController::class, 'destroy']);
 
         // 게시글 CRUD
-        Route::get('/{boardId}/post', [PostController::class, 'index']);
-        Route::get('/{boardId}/post/{id}', [PostController::class, 'show']);
+        Route::post('/{boardId}/post', [PostController::class, 'store']);
+        Route::get('/{boardId}/post', [PostController::class, 'index'])->withoutmiddleware('auth:api');
+        Route::get('/{boardId}/post/{id}', [PostController::class, 'show'])->withoutmiddleware('auth:api');
+        Route::patch('/{boardId}/post/{id}', [PostController::class, 'update']);
+        Route::delete('/{boardId}/post/{id}', [PostController::class, 'destroy']);
 
         // 댓글 CRUD
-        Route::get('/{boardId}/post/{postId}/reply', [ReplyController::class, 'index']);
+        Route::post('/{boardId}/post/{postId}/reply', [ReplyController::class, 'store']);
+        Route::get('/{boardId}/post/{postId}/reply', [ReplyController::class, 'index'])->withoutmiddleware('auth:api');
+        Route::patch('/{boardId}/post/{postId}/reply/{id}', [ReplyController::class, 'update']);
+        Route::delete('/{boardId}/post/{postId}/reply/{id}', [ReplyController::class, 'destroy']);
 
+        // Unique API
+        // 게시판의 게시글 수를 포함한 목록
+        Route::get('/posts-count', [BoardController::class, 'getPostsCount']);
 
-        // 인증 필요
-        Route::group([
-            'middleware' => 'auth:api',
-        ], function () {
-            // 게시판의 게시글 수를 포함한 목록
-            Route::get('/posts-count', [BoardController::class, 'getPostsCount']);
-
-            // 게시판의 전시순서 변경
-            Route::patch('/{id}/sort', [BoardController::class, 'updateBoardSort']);
-
-            // 게시판 CRUD
-            Route::post('', [BoardController::class, 'store']);
-            Route::patch('/{id}', [BoardController::class, 'update']);
-            Route::delete('/{id}', [BoardController::class, 'destroy']);
-
-            /**
-             * 게시글 CRUD
-             */
-            Route::post('/{boardId}/post', [PostController::class, 'store']);
-            Route::patch('/{boardId}/post/{id}', [PostController::class, 'update']);
-            Route::delete('/{boardId}/post/{id}', [PostController::class, 'destroy']);
-
-            // 댓글 CRUD
-            Route::post('/{boardId}/post/{postId}/reply', [ReplyController::class, 'store']);
-            Route::patch('/{boardId}/post/{postId}/reply/{id}', [ReplyController::class, 'update']);
-            Route::delete('/{boardId}/post/{postId}/reply/{id}', [ReplyController::class, 'destroy']);
-        });
-
+        // 게시판의 전시순서 변경
+        Route::patch('/{id}/sort', [BoardController::class, 'updateBoardSort']);
     });
 
     // 게시판 옵션 관련
@@ -202,40 +123,21 @@ Route::group([
         'only' => ['index']
     ]);
 
-
-    // 1:1 문의
-    Route::group([
-        'prefix' => 'inquiry',
-        'middleware' => 'auth:api',
-    ], function () {
-
-        // 문의 목록
-        Route::get('', [InquiryController::class, 'index']);
-
-        // 문의 상세정보
-        Route::get('/{inquiryId}', [InquiryController::class, 'show']);
-
-        // 문의 작성
+    /**
+     * 1:1 문의
+     */
+    Route::group(['prefix' => 'inquiry', 'middleware' => 'auth:api'], function () {
+        // 문의 CRUD
         Route::post('', [InquiryController::class, 'store']);
-
-        // 문의 수정
+        Route::get('', [InquiryController::class, 'index']);
+        Route::get('/{inquiryId}', [InquiryController::class, 'show']);
         Route::patch('{inquiryId}', [InquiryController::class, 'update']);
-
-        // 문의 삭제
         Route::delete('{inquiryId}', [InquiryController::class, 'destroy']);
 
-        // 답변 상세정보 및 작성
-        Route::resource('{inquiryId}/answer', InquiryAnswerController::class, [
-            'only' => ['store']
-        ]);
-
-        // 답변 상세정보 (Customized Router)
+        // 답변 CRUD (Customized Router)
+        Route::post('{inquiryId}/answer', [InquiryAnswerController::class, 'store']);
         Route::get('{inquiryId}/answer', [InquiryAnswerController::class, 'show']);
-
-        // 답변 수정 (Customized Router)
         Route::patch('{inquiryId}/answer', [InquiryAnswerController::class, 'update']);
-
-        // 답변 삭제 (Customized Router)
         Route::delete('{inquiryId}/answer', [InquiryAnswerController::class, 'destroy']);
     });
 
