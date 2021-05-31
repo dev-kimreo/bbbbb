94b7478b1074df8f229e7413c5f3008b542b643b
dev-kimreo0301 <wkim0301@cocen.com>
2021-05-31 12:29:34 +0900

    fix: use Auth 추가

--- app/Http/Requests/Boards/UpdateBoardSortRequest.php
+++ app/Http/Requests/Boards/UpdateBoardSortRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Boards;
 
+use Auth;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Http\Exceptions\HttpResponseException;
