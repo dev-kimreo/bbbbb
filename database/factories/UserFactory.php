7a2f3a2a458a6676d7a37a88ba4f7bc40e531750
dev-kimreo0301 <wkim0301@cocen.com>
2021-06-16 13:57:43 +0900

    feat: User Controller Feature Test 작성 완료

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,7 +25,6 @@ class UserFactory extends Factory
         return [
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
-            'email_verified_at' => now(),
             'password' => 'b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86', // password
             'remember_token' => Str::random(10),
         ];
