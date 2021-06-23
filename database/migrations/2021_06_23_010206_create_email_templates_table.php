05b3fd6e30cddd2108d258b6f0b1b3280eecfabe
dev-kimreo0301 <wkim0301@cocen.com>
2021-06-23 12:18:05 +0900

    feat: 이메일 템플릿 모델 및 migration 추가

---
+++ database/migrations/2021_06_23_010206_create_email_templates_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmailTemplatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('email_templates', function (Blueprint $table) {
+            $table->collation = 'utf8mb4_general_ci';
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('code', 64);
+            $table->string('name', 256);
+            $table->boolean('enable');
+            $table->string('title', 256);
+            $table->text('content');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('email_templates');
+    }
+}
