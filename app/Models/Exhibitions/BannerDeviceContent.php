e79f1346570a7fb1be698667c343a45321c048ba
Jaewon Kim <epiloum@gmail.com>
2021-07-20 12:14:47 +0900

    feat: add the getParentRelation() method on models

--- app/Models/Exhibitions/BannerDeviceContent.php
+++ app/Models/Exhibitions/BannerDeviceContent.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
@@ -44,5 +45,10 @@ class BannerDeviceContent extends Model
     {
         $this->attachFile()->delete();
         return parent::delete();
+    }
+
+    public function getParentRelation(): Relation
+    {
+        return $this->banner();
     }
 }
