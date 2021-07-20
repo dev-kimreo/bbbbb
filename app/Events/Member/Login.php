34dfb87035e6c502e318fe73c22739cd1aa89ff9
Jaewon Kim <epiloum@gmail.com>
2021-07-20 14:51:45 +0900

    feat: add properties to login and logout events

--- app/Events/Member/Login.php
+++ app/Events/Member/Login.php
@@ -2,9 +2,11 @@
 
 namespace App\Events\Member;
 
+use App\Models\User;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Http\Request;
 use Illuminate\Queue\SerializesModels;
@@ -13,11 +15,14 @@ class Login
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
+    public Model $model;
+    public int $id;
     public int $user_id;
     public int $user_grade;
     public ?int $manager_id = null;
     public int $client_id;
     public ?string $ip;
+    public string $title;
     public ?array $properties = [];
 
     /**
@@ -27,11 +32,14 @@ class Login
      */
     public function __construct(Request $req, int $user_id, int $user_grade, int $client_id, int $manager_id = null)
     {
+        $this->model = User::find($user_id);
+        $this->id = $user_id;
         $this->user_id = $user_id;
         $this->user_grade = $user_grade;
         $this->manager_id = $manager_id;
         $this->client_id = $client_id;
         $this->ip = $req->ip();
+        $this->title = ($this->manager_id? '관리자 ': '') . '로그인';
     }
 
     /**
