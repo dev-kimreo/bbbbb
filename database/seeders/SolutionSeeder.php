f667713b8ecea7900892281dce1c81418e09ff50
dev-kimreo0301 <wkim0301@cocen.com>
2021-09-13 15:55:06 +0900

    feat: Core Entity 중 솔루션의 Model, Migration, Seeder 추가

---
+++ database/seeders/SolutionSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SolutionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('solutions')->insert([
+
+            [
+                'name' => '메이크샵',
+                'created_at' => Carbon::now()
+            ],
+            [
+                'name' => '마이소호',
+                'created_at' => Carbon::now()
+            ]
+        ]);
+    }
+}
