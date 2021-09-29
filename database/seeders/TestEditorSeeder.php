1035df070b88636328c8979581f9ae65c8c94179
dev-kimreo0301 <wkim0301@cocen.com>
2021-09-29 09:24:52 +0900

    refac: 변경사항 seeder 에 반영 (theme_goods 네이밍 변경, 컴포넌트 유형의 속성 추가, 테마 상품의 전시정보 추가)

--- database/seeders/TestEditorSeeder.php
+++ database/seeders/TestEditorSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Components\Component;
 use App\Models\Components\ComponentOption;
 use App\Models\Components\ComponentType;
+use App\Models\Components\ComponentTypeProperty;
 use App\Models\Components\ComponentVersion;
 use App\Models\EditablePages\EditablePage;
 use App\Models\EditablePages\EditablePageLayout;
@@ -15,7 +16,8 @@ use App\Models\Manager;
 use App\Models\Solution;
 use App\Models\SupportedEditablePage;
 use App\Models\Themes\Theme;
-use App\Models\Themes\ThemeGood;
+use App\Models\Themes\ThemeProduct;
+use App\Models\Themes\ThemeProductInformation;
 use App\Models\Users\User;
 use App\Models\Users\UserPartner;
 use Illuminate\Database\Seeder;
@@ -45,13 +47,22 @@ class TestEditorSeeder extends Seeder
         }
 
         // 컴포넌트 텍스트 유형 추가
-        ComponentType::firstOrCreate(
-            ['type' => 'text'],
+        $componentType = ComponentType::firstOrCreate(
+            ['code' => 'text'],
             [
                 'name' => '텍스트형',
-                'html' => '<div><input type="text" name="::key::" value="::value::" /></div>',
-                'style' => '',
-                'script' => '',
+                'isPlural' => false
+            ]
+        );
+
+        // 컴포넌트 텍스트 옵션 유형 속성 추가
+        ComponentTypeProperty::firstOrCreate(
+            ['component_type_id' => $componentType->id],
+            [
+                'type' => 'text',
+                'hasOption' => false,
+                'hasDefault' => true,
+                'default' => '텍스트필드'
             ]
         );
 
@@ -84,9 +95,12 @@ class TestEditorSeeder extends Seeder
         ])->create();
 
         // 테마 상품 생성
-        ThemeGood::factory()->for(
+        ThemeProduct::factory()->for(
             UserPartner::first(),
             'creator'
+        )->has(
+            ThemeProductInformation::factory(),
+            'themeInformation'
         )->has(
             // 테마
             Theme::factory()->for(
