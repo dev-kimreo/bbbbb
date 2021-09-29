f1c4fd0a6c43f0d65f26f2cd22b1e7780dbf8297
dev-kimreo0301 <wkim0301@cocen.com>
2021-09-29 09:23:43 +0900

    feat: component 유형의 속성 추가

--- database/migrations/2021_09_14_232020_create_component_type_properties_table.php
+++ database/migrations/2021_09_14_232020_create_component_type_properties_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateComponentTypesTable extends Migration
+class CreateComponentTypePropertiesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,14 @@ class CreateComponentTypesTable extends Migration
      */
     public function up()
     {
-        Schema::create('component_types', function (Blueprint $table) {
+        Schema::create('component_type_properties', function (Blueprint $table) {
             $table->collation = 'utf8mb4_general_ci';
             $table->id();
-            $table->string('name', 64);
-            $table->string('code', 64);
-            $table->boolean('isPlural');
+            $table->foreignId('component_type_id')->constrained();
+            $table->enum('type', ['boolean', 'integer', 'file', 'text']);
+            $table->boolean('hasOption');
+            $table->boolean('hasDefault');
+            $table->string('default');
             $table->timestamps();
             $table->softDeletes();
         });
@@ -31,6 +33,6 @@ class CreateComponentTypesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('component_types');
+        Schema::dropIfExists('component_type_properties');
     }
 }
