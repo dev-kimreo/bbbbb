8fbc3e97efcc511c975c12ea4922f5a960534ce6
Jaewon Kim <epiloum@cocen.com>
2021-10-01 12:15:19 +0900

    feat: add component upload images migration

---
+++ database/migrations/2021_10_01_012745_create_component_upload_images_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComponentUploadImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('component_upload_images', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('attach_file_id')->constrained();
+            $table->string('url_thumb');
+            $table->integer('width');
+            $table->integer('height');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('component_upload_images');
+    }
+}
