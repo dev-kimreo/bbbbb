b129833c2e06383aad572409826fe9b8a3e0208d
dev-kimreo0301 <wkim0301@cocen.com>
2021-10-12 09:42:25 +0900

    refac: theme swagger schema 추가

--- app/Models/Themes/Theme.php
+++ app/Models/Themes/Theme.php
@@ -11,9 +11,17 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+
 /**
  *
  * @OA\Schema(
+ *      @OA\Property(property="id", type="integer", example=1, description="고유번호"),
+ *      @OA\Property(property="theme_product_id", type="integer", example=1, description="테마 상품 고유 번호"),
+ *      @OA\Property(property="solution_id", type="integer", example=1, description="솔루션 고유 번호"),
+ *      @OA\Property(property="status", type="string", example="registering", description="테마 상태"),
+ *      @OA\Property(property="display", type="boolean", example=false, default=false, description="노출 여부"),
+ *      @OA\Property(property="createdAt", type="string", format="date-time", description="등록 일자"),
+ *      @OA\Property(property="updatedAt", type="string", format="date-time", description="수정 일자")
  * )
  *
  * @method where(array $array)
