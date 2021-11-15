0b314bcf8841e0d0260172b5d11acfc0079f509d
dev-kimreo0301 <wkim0301@cocen.com>
2021-11-15 16:27:48 +0900

    feat: Component 관련 CRUD Request 추가

---
+++ app/Http/Requests/Components/StoreRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests\Components;
+
+use App\Models\Components\Component;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use App\Rules\Components\ComponentUsagePageBySolution;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'solution_id' => ['required', 'integer', 'exists:App\Models\Solution,id'],
+            'name' => ['required', 'string'],
+            'use_other_than_maker' => ['prohibited'],
+            'first_category' => ['required', 'string', Rule::in(array_keys(Component::$firstCategory))],
+            'second_category' => ['exclude_if:first_category,theme_component', 'required', 'string', Rule::in(array_keys(Component::$secondCategory))],
+            'use_blank' => ['required', 'boolean'],
+            'use_all_page' => ['required', 'boolean', new ComponentUsagePageBySolution],
+            'display' => ['required', 'boolean'],
+            'status' => ['required', 'string', Rule::in(array_keys(Component::$status))],
+            'manager_memo' => ['sometimes', 'string'],
+        ];
+    }
+}
---
+++ app/Http/Requests/Components/UpdateRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests\Components;
+
+use App\Models\Components\Component;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use App\Rules\Components\ComponentUsagePageBySolution;
+
+class UpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'solution_id' => ['sometimes', 'integer', 'exists:App\Models\Solution,id'],
+            'name' => ['sometimes', 'string'],
+            'use_other_than_maker' => ['prohibited'],
+            'first_category' => ['sometimes', 'string', Rule::in(array_keys(Component::$firstCategory))],
+            'second_category' => ['exclude_unless:first_category,design,product,solution,', 'required', 'string', Rule::in(array_keys(Component::$secondCategory))],
+            'use_blank' => ['sometimes', 'boolean'],
+            'use_all_page' => ['sometimes', 'boolean', new ComponentUsagePageBySolution],
+            'display' => ['sometimes', 'boolean'],
+            'status' => ['sometimes', 'string', Rule::in(array_keys(Component::$status))],
+            'manager_memo' => ['sometimes', 'string'],
+        ];
+    }
+}
