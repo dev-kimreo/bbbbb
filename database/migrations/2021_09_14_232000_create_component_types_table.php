5e1060a8c861a465bfc34eca6712aa4b2901706b
dev-kimreo0301 <wkim0301@cocen.com>
2021-12-02 08:43:42 +0900

    feat: 컴포넌트 옵션 유형 테이블에서 유형코드(code) 제거, 사용가능한 상세설정 옵션(attributes) 추가

--- database/migrations/2021_09_14_232000_create_component_types_table.php
+++ database/migrations/2021_09_14_232000_create_component_types_table.php
@@ -17,9 +17,9 @@ class CreateComponentTypesTable extends Migration
             $table->collation = 'utf8mb4_general_ci';
             $table->id();
             $table->string('name', 64);
-            $table->string('code', 64);
             $table->boolean('isPlural');
             $table->unsignedInteger('maxCount')->default(1);
+            $table->text('attributes')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
