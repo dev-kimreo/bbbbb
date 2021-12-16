9090aba0f4039d70a2fccf9b8cbb8e8c20133564
Jaewon Kim <epiloum@cocen.com>
2021-12-16 16:24:08 +0900

    feat: add jsonp method

--- app/Http/Controllers/LinkedComponents/ScriptRequestController.php
+++ app/Http/Controllers/LinkedComponents/ScriptRequestController.php
@@ -11,11 +11,19 @@ class ScriptRequestController extends Controller
 
     /**
      * @OA\Get (
-     *      path="/v1/component/script/{hash}",
+     *      path="/v1/component/script/{hash}.js",
      *      summary="컴포넌트 스크립트 요청",
      *      description="설명: https://www.notion.so/Component-Rendering-d474a80d2291486f83551058c2e9121d#9e0a0deeae044389a7784160531202da",
      *      operationId="ComponentScriptRequest",
      *      tags={"연동 컴포넌트"},
+     *      @OA\RequestBody(
+     *          required=true,
+     *          description="",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="type", type="string", example="module", description="반환할 스크립트 형식<br/>module:모듈방식(기본값), jsonp:JSONP 방식"),
+     *              @OA\Property(property="callback", type="string", example="render", description="JSONP 방식 선택시, 콜백함수의 이름")
+     *          )
+     *      ),
      *      @OA\Response(
      *          response=200,
      *          description="successfully"
@@ -31,7 +39,13 @@ class ScriptRequestController extends Controller
      */
     public function show(Request $req, string $hash)
     {
-        return response(ComponentRenderingService::getScript($hash), 200)
+        if($req->input('type') == 'jsonp') {
+            $code = ComponentRenderingService::getJsonp($hash, $req->input('callback') ?? 'render');
+        } else {
+            $code = ComponentRenderingService::getScript($hash);
+        }
+
+        return response($code, 200)
             ->header('Content-Type', 'application/javascript');
     }
 }
