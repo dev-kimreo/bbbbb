f5c10c648512afc9a4c03ee93a3fd3f9102798f9
dev-kimreo0301 <wkim0301@cocen.com>
2022-01-04 09:23:54 +0900

    feat: word entity 추가 ( 다국어 단어 entity )

---
+++ database/migrations/2021_06_14_052718_create_words_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('words', function (Blueprint $table) {
+            $table->id();
+            $table->string('code', 64);
+            $table->string('title', 256);
+            $table->timestamps();
+            $table->softDeletes();
+            $table->index('code');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('words');
+    }
+}
