bd0ad96c6976ffa152bbb9c9a6b3f04e3f4d5359
dev-kimreo0301 <wkim0301@cocen.com>
2022-01-10 08:30:44 +0900

    feat: word crud, non-curd api 및 swagger 개발, Request 추가

---
+++ app/Http/Requests/Words/RelationStoreRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Words;
+
+use App\Rules\ArrayKeysInIso639_1;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class RelationStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'code' => ['required', Rule::unique('App\Models\Word', 'code')->where(function ($q){
+                return $q->where('deleted_at', NULL);
+            })],
+            'title' => ['required', 'string'],
+            'value' => ['required', 'array', new ArrayKeysInIso639_1],
+        ];
+    }
+}
