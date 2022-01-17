840b3f0e3c7d0901d3aa6482d062777619c44e54
dev-kimreo0301 <wkim0301@cocen.com>
2022-01-17 16:21:44 +0900

    fix: component table migrate 수정 ( manager_memo nullable 추가 및 default 값 추가)

--- database/migrations/2021_09_14_232050_create_components_table.php
+++ database/migrations/2021_09_14_232050_create_components_table.php
@@ -22,12 +22,12 @@ class CreateComponentsTable extends Migration
             $table->boolean('use_other_than_maker');            // 제작자 외 회원 사용가능 여부
             $table->string('first_category');                   // 컴포넌트 카테고리1
             $table->string('second_category')->nullable();      // 컴포넌트 카테고리2
-            $table->boolean('use_blank');                       // 여백 옵션
+            $table->boolean('use_blank')->default(0);                       // 여백 옵션
             $table->boolean('use_all_page');                    // 사용 페이지 타입 (전체 = true, 선택 = false)
             $table->string('icon', 64);                     // 컴포넌트 아이콘
-            $table->boolean('display');                         // 노출 여부
+            $table->boolean('display')->default(0);                         // 노출 여부
             $table->string('status')->default('registering');   // 상태 구분 (등록중, 등록완료)
-            $table->text('manager_memo');                       // 관리자 메모
+            $table->text('manager_memo')->nullable();                       // 관리자 메모
             $table->timestamps();
             $table->softDeletes();
         });
