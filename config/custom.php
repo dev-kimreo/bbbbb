d0bf9888d7f9c4b9035d47f6e864fbcd1fb838c2
Jaewon Kim <epiloum@cocen.com>
2022-01-24 11:15:29 +0900

    feat: add membership pricing type

--- config/custom.php
+++ config/custom.php
@@ -6,6 +6,11 @@ return [
         'toInactiveDays' => 365,
         'toAutoWithdrawalDays' => 365,
         'toDestructDays' => 90
+        'toDestructDays' => 90,
+        'pricingType' => [
+            'free' => 0,
+            'standard' => 1
+        ]
     ],
 
 ];
