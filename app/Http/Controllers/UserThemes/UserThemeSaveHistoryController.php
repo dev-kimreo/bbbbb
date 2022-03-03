16506b3eb975b92119e682f151ae03f8cb7118de
Jaewon Kim <epiloum@gmail.com>
2022-03-03 15:05:14 +0900

    feat: controller

---
+++ app/Http/Controllers/UserThemes/UserThemeSaveHistoryController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\UserThemes;
+
+use App\Exceptions\QpickHttpException;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UserThemes\SaveHistory\StoreRequest;
+use App\Models\UserThemes\UserTheme;
+use App\Models\UserThemes\UserThemeSaveHistory;
+use Auth;
+use Illuminate\Http\Response;
+use Illuminate\Support\Collection;
+
+class UserThemeSaveHistoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @param int $userThemeId
+     * @return Collection
+     * @throws QpickHttpException
+     */
+    public function index(int $userThemeId): Collection
+    {
+        $this->chkAuth($userThemeId);
+
+        return collect(UserThemeSaveHistory::query()->where('user_theme_id', $userThemeId));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param StoreRequest $request
+     * @param int $userThemeId
+     * @return Collection
+     * @throws QpickHttpException
+     */
+    public function store(StoreRequest $request, int $userThemeId): Collection
+    {
+        $this->chkAuth($userThemeId);
+
+        $res = UserThemeSaveHistory::query()->create(
+            [
+                'user_theme_id' => $userThemeId,
+                'data' => $request->data
+            ]
+        );
+
+        return collect(UserThemeSaveHistory::query()->find($res->id));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $userThemeId
+     * @param int $userThemeSaveHistoryId
+     * @return Response
+     * @throws QpickHttpException
+     */
+    public function destroy(int $userThemeId, int $userThemeSaveHistoryId): Response
+    {
+        $this->chkAuth($userThemeId);
+        UserThemeSaveHistory::query()->findOrFail($userThemeSaveHistoryId)->delete();
+        return response()->noContent();
+    }
+
+    /**
+     * @throws QpickHttpException
+     */
+    protected function chkAuth($userThemeId)
+    {
+        $userTheme = UserTheme::query()->findOrFail($userThemeId);
+
+        if (!Auth::isLoggedForBackoffice() && $userTheme->user_id != $userThemeId) {
+            throw new QpickHttpException(403, 'common.unauthorized');
+        }
+    }
+}
